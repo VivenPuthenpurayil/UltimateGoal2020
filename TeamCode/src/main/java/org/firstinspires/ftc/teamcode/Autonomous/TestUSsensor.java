3324a36704b3e44318dbfdf9fd5c32adcc990077
Jimmy <jimmybao22@gmail.com>
2020-12-12 20:30:45 -0500

    jimmy bao

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestUSsensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestUSsensor.java
@@ -21,13 +21,13 @@ public class TestUSsensor extends AutonomousControl {
 //            telemetry.addData("Raw", rob.leftSense.getRawLightDetected());
 //            telemetry.update();
 //            sleep(20000);
-    telemetry.addData("raw ultrasonic", rob.leftSense.rawUltrasonic());
-    telemetry.addData("raw optical", rob.leftSense.rawOptical());
-    telemetry.addData("cm optical", "%.2f cm", rob.leftSense.cmOptical());
-    telemetry.addData("cm", "%.2f cm", rob.leftSense.getDistance(DistanceUnit.CM));
-    // sleep(1000);
-    telemetry.update();
-    sleep(2000);
+            telemetry.addData("raw ultrasonic", rob.leftSense.rawUltrasonic());
+            telemetry.addData("raw optical", rob.leftSense.rawOptical());
+            telemetry.addData("cm optical", "%.2f cm", rob.leftSense.cmOptical());
+            telemetry.addData("cm", "%.2f cm", rob.leftSense.getDistance(DistanceUnit.CM));
+            // sleep(1000);
+            telemetry.update();
+            sleep(2000);
 
         }
     }
