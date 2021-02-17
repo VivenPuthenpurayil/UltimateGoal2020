9e6e31ac633d311d36f2efebc94fe7b7b2a32867
Aniket-Rai1 <ridge.arai@gmail.com>
2021-02-17 18:53:00 -0500

    kuberdum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JanuaryAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JanuaryAuton.java
@@ -71,7 +71,7 @@ public class JanuaryAuton extends AutonomousControl
 
         if (opModeIsActive())
         {
-            //rob.driveTrainEncoderMovement(0.5, 10, 4, 0, Goal.movements.forward);
+            rob.driveTrainEncoderMovement(0.5, 30, 4, 0, Goal.movements.forward);
 
             while(runtime.milliseconds() < 5000) {
                 telemetry.addData("Analysis", pipeline.getAnalysis());
@@ -82,13 +82,106 @@ public class JanuaryAuton extends AutonomousControl
                 // Don't burn CPU cycles busy-looping in this sample
                 sleep(50);
             }
+            int dist;
             if (pipeline.value == 4){
-
+                dist = 114;
             }else if(pipeline.value == 1){
-
+                dist = 90;
             }else{
+                dist = 66;
 
             }
+            //move to red square
+            rob.driveTrainEncoderMovement(1,dist,20,0,Goal.movements.forward);
+
+            //drop wobble goal
+            rob.pinch.setPosition(0.8);
+            sleep(500);
+            rob.claw.setPower(0.3);
+            sleep(500);
+            rob.claw.setPower(0);
+            sleep(250);
+
+
+            //move back to pick up second wobble goal
+
+            rob.driveTrainEncoderMovement(1,dist - 10,20,0,Goal.movements.backward);
+
+            rob.lifter.setPosition(.84);
+            sleep(200);
+
+            rob.driveTrainEncoderMovement(1,6,20,0,Goal.movements.left);
+
+            // turn to face second wobble goal
+            rob.driveTrainEncoderMovement(1,23,20,0,Goal.movements.cw);
+
+            rob.pinch.setPosition(0.8);
+            sleep(500);
+
+            // move to second wobble goal
+            rob.driveTrainEncoderMovement(.75,16,20,0,Goal.movements.forward);
+
+            // pick up second wobble goal
+            sleep(250);
+            rob.claw.setPower(-0.3);
+            sleep(300);
+            rob.claw.setPower(0);
+            sleep(500);
+            rob.pinch.setPosition(0);
+            sleep(400);
+            rob.claw.setPower(-0.4);
+            sleep(350);
+            rob.claw.setPower(0);
+            sleep(250);
+
+            // move backwards to go back to red square
+            rob.driveTrainEncoderMovement(1,23,20,0,Goal.movements.ccw);
+            rob.driveTrainEncoderMovement(1,23,20,0,Goal.movements.right);
+            rob.driveTrainEncoderMovement(1,42,20,0,Goal.movements.forward);
+
+            //drop wobble goal
+            rob.pinch.setPosition(0.8);
+            sleep(250);
+            rob.claw.setPower(0.3);
+            sleep(500);
+            rob.claw.setPower(0);
+            sleep(250);
+
+            // start flywheel
+//          rob.fly.setPower(-0.635);
+            rob.fly.setPower(-0.73);
+            sleep(2000);
+
+            // move backwards a bit so you dont hit the wobble goal
+            rob.driveTrainEncoderMovement(1,7,20,0,Goal.movements.backward);
+
+            // move towards wall
+            // rob.driveTrainEncoderMovement(1,9,20, 0, Goal.movements.right);
+
+            // move to the left, to align shots
+//          rob.driveTrainEncoderMovement(1,44,20,0,Goal.movements.left);
+            rob.driveTrainEncoderMovement(1,22 ,20,0,Goal.movements.left);
+
+            // move to right behind white line
+            rob.driveTrainEncoderMovement(1,9,20,0,Goal.movements.forward);
+
+
+
+            sleep(500);
+            for(int i = 0; i<=2; i++) {
+                rob.fly.setPower(-0.73);
+                sleep(200);
+                //200
+                rob.whack.setPosition(0.62);
+                sleep(1000);
+                //1000 each
+                rob.whack.setPosition(0);
+                sleep(1000);
+            }
+
+            // move to Launch Line
+            rob.driveTrainEncoderMovement(1,8, 100, 100,Goal.movements.forward);
+
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CurrentTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CurrentTeleop.java
@@ -13,6 +13,8 @@ public class CurrentTeleop extends TeleOpControl {
     public static final double rotationSpeed = 0.4;
     public static boolean flywheelon = false;
 
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         boolean yToggle = false;
@@ -178,7 +180,7 @@ public class CurrentTeleop extends TeleOpControl {
                         break;
                     }
                 }
-                rob.fly.setPower(0);
+//                rob.fly.setPower(0);
                 rob.lifter.setPosition(.98);
                 sleep(200);
             }
@@ -206,18 +208,11 @@ public class CurrentTeleop extends TeleOpControl {
                 if (distanceBack > 1000 || distanceFront > 1000) {
                     continue;
                 }
-
                 angle = (Math.atan((distanceFront - distanceBack) / 6.6142) * 180) / (3.1415);
-                rob.driveTrainEncoderMovement(.5, 12.75 / 90 * angle, 10, 10, Goal.movements.ccw);
+                rob.turn((float) (Math.abs(angle)), Goal.turnside.ccw, 0.5, Goal.axis.center);
 
-                while (true) {
-                    if (rob.Back.getDistance((DistanceUnit.CM)) > 1000){
-                        continue;
-                    }
-                    if (rob.Back.getDistance((DistanceUnit.CM)) < 145){
-                        rob.driveTrainMovement(0.5, Goal.movements.forward);
-                    }
-                    else break;
+                while (rob.Back.getDistance((DistanceUnit.CM))< 156){
+                    rob.driveTrainMovement(.5, Goal.movements.forward);
                 }
 
                 int error_dist = 5;
@@ -237,25 +232,24 @@ public class CurrentTeleop extends TeleOpControl {
                         rob.driveTrainMovement(0.3, Goal.movements.right);
                     }
                 }
-
                 move_to_pos = false;
             }
 
 
-            /*
-                if(gamepad2.dpad_up){
-                    rob.rightLinear.setPower(1);
-                }
-                else if (gamepad2.dpad_down){
-                    rob.rightLinear.setPower(-0.5);
-                }
-                else {
-                    rob.rightLinear.setPower(0);
-                }
+    /*
+        if(gamepad2.dpad_up){
+            rob.rightLinear.setPower(1);
+        }
+        else if (gamepad2.dpad_down){
+            rob.rightLinear.setPower(-0.5);
+        }
+        else {
+            rob.rightLinear.setPower(0);
+        }
 
-            */
+  */
 
-            //check point`
+//check point
 
         }
     }
