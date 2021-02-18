d7bfa6221decdc3dceec7cd82cebfedc1fd93130
Viven Puthenpurayil <vivenputhenpurayil@gmail.com>
2021-02-17 19:08:14 -0500

    Revert "hi"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Goal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/Goal.java
@@ -72,7 +72,7 @@ public class Goal {
         this.hardwareMap = hardwareMap;
         this.runtime = runtime;
         this.central = central;
-
+//
 
         StringBuilder i = new StringBuilder();
 
